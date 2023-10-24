0d78dea6db3b72888d409808ef3e94f2ffa178cd
Joshua Meadows <jtmeadows@mail.wvu.edu>
2023-10-24 11:24:07 -0400

    System generated (ignore)

--- ProductsByCategory.aspx.designer.cs
+++ ProductsByCategory.aspx.designer.cs
@@ -30,6 +30,6 @@ namespace SportsStore
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView Products;
+        protected global::System.Web.UI.WebControls.Repeater Products;
     }
 }
