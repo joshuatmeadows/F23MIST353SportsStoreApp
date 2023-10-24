8f5b8a24c5e0131416d162b1a32f9262ebe5c41d
Joshua Meadows <jtmeadows@mail.wvu.edu>
2023-10-24 10:47:37 -0400

    System generated (ignore)

--- ProductsByCategory.aspx.designer.cs
+++ ProductsByCategory.aspx.designer.cs
@@ -11,16 +11,25 @@ namespace SportsStore
 {
 
 
-    public partial class _Default
+    public partial class ProductsByCategory
     {
 
         /// <summary>
-        /// Categories control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater Categories;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Products control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView Products;
     }
 }
