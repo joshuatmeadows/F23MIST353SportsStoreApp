3072035e722ea2439380fbaa339369a314e5b9fd
Joshua Meadows <jtmeadows@mail.wvu.edu>
2023-10-24 13:03:24 -0400

    system generated (ignore)

--- Product.aspx.designer.cs
+++ Product.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace SportsStore
     {
 
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
         /// lblName control.
         /// </summary>
         /// <remarks>
@@ -31,6 +22,15 @@ namespace SportsStore
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblName;
+
+        /// <summary>
+        /// imgImage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgImage;
 
         /// <summary>
         /// lblDescription control.
