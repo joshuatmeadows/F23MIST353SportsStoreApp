00d353aa90825ae07092b3e625d83469bc9c0424
Joshua Meadows <jtmeadows@mail.wvu.edu>
2023-11-09 13:15:15 -0500

    System Generated (ignore)

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -15,6 +15,15 @@ namespace SportsStore
     {
 
         /// <summary>
+        /// LoginView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginView LoginView1;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
